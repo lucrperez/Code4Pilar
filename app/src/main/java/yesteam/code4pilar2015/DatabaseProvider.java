899a94d7c9dcf33e21adc3b6d166c83991c9921b
kix2902 <ismael.kix2902@gmail.com>
2015-09-21 22:18:42 +0200

    Added ContentProvider class

---
+++ app/src/main/java/yesteam/code4pilar2015/DatabaseProvider.java
@@ -0,0 +1,9 @@
+package yesteam.code4pilar2015;
+
+import novoda.lib.sqliteprovider.provider.SQLiteContentProviderImpl;
+
+public class DatabaseProvider extends SQLiteContentProviderImpl {
+
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".provider";
+
+}
