05b17d4bd481e14c5b4f571b4f7716fe40e7fc31
Luis Cruz <lucrperez@gmail.com>
2015-09-24 20:12:32 +0200

    Main Activity Image improvements. Links Event image to Event List Activity

--- app/src/main/java/yesteam/code4pilar2015/activities/MainActivity.java
+++ app/src/main/java/yesteam/code4pilar2015/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package yesteam.code4pilar2015.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
 
 import yesteam.code4pilar2015.R;
 import yesteam.code4pilar2015.adapters.EventsAdapter;
@@ -19,6 +22,47 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Button Ofrenda
+        ImageView img1 = (ImageView) findViewById(R.id.main_img_ofrenda);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent int_ofrenda = new Intent(MainActivity.this, OfrendaListActivity.class);
+                //startActivity(int_ofrenda);
+            }
+        });
+
+        //Button Eventos
+        ImageView img2 = (ImageView) findViewById(R.id.main_img_events);
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int_events = new Intent(MainActivity.this, EventsListActivity.class);
+                startActivity(int_events);
+            }
+        });
+
+        //Button Locations
+        ImageView img3 = (ImageView) findViewById(R.id.main_img_locations);
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent int_locations = new Intent(MainActivity.this, LocationsListActivity.class);
+                //startActivity(int_locations);
+            }
+        });
+
+        //Button Artists
+        ImageView img4 = (ImageView) findViewById(R.id.main_img_artists);
+        img4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent int_artists = new Intent(MainActivity.this, ArtistsListActivity.class);
+                //startActivity(int_artists);
+            }
+        });
+
 /*
         RecyclerView mRecyclerViewLeft = (RecyclerView) findViewById(R.id.recycler_view_left);
         mRecyclerViewLeft.setHasFixedSize(true);
