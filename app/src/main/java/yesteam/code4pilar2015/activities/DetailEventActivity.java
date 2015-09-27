789ace457570d79ee04ff1768035ad301cbf976a
kix2902 <ismael.kix2902@gmail.com>
2015-09-28 00:49:39 +0200

    Created detail event layout and attached to activity

--- app/src/main/java/yesteam/code4pilar2015/activities/DetailEventActivity.java
+++ app/src/main/java/yesteam/code4pilar2015/activities/DetailEventActivity.java
@@ -1,6 +1,20 @@
 package yesteam.code4pilar2015.activities;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import yesteam.code4pilar2015.R;
 
 public class DetailEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_event);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
 }
