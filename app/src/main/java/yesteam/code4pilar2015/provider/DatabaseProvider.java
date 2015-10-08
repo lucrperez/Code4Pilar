263b4469cf9e35414b0d72e44f17f517a94c0185
kix2902 <ismael.kix2902@gmail.com>
2015-10-09 00:21:44 +0200

    Added column to get event's population type

--- app/src/main/java/yesteam/code4pilar2015/provider/DatabaseProvider.java
+++ app/src/main/java/yesteam/code4pilar2015/provider/DatabaseProvider.java
@@ -53,6 +53,7 @@ public class DatabaseProvider extends SQLiteContentProviderImpl {
         public static final String COLUMN_PRICE = "price";
         public static final String COLUMN_WEB = "web";
         public static final String COLUMN_IMAGE = "image";
+        public static final String COLUMN_POPULATION_TYPE = "population_type";
 
         public static final Uri URI = Uri.parse(AUTHORITY).buildUpon().appendPath(TABLE_NAME).build();
     }
